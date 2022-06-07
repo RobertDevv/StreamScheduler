ed72f596fb891ca2088db5d8fce5f925184da918
Robert <94220124+RobertDevv@users.noreply.github.com>
2022-06-07 19:59:53 +0100

    Add project files.

---
+++ Data/DatabaseContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ScheduleWeb.Data.Models;
+
+namespace ScheduleWeb.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<Schedule> Schedule { get; set; }
+        public DbSet<Logins> Logins { get; set; }
+        public string DbPath { get; set; }
+
+        public DatabaseContext()
+        {
+#if DEBUG
+            DbPath = Directory.GetCurrentDirectory() + "\\db\\ScheduleWeb.db";
+#else
+            DbPath = Directory.GetCurrentDirectory() + "/db/ScheduleWeb.db";
+#endif
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlite($"Data Source={DbPath}");
+
+    }
+}
---
+++ Data/Models/Schedule.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ScheduleWeb.Data.Models
+{
+    public class Schedule
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+        public TimeSpan Monday { get; set; }
+        public TimeSpan Tuesday { get; set; }
+        public TimeSpan Wednesday { get; set; }
+        public TimeSpan Thursday { get; set; }
+        public TimeSpan Friday { get; set; }
+        public TimeSpan Saturday { get; set; }
+        public TimeSpan Sunday { get; set; }
+
+    }
+}
